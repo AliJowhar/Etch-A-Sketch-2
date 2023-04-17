b2f069bdad33a021bf598dcb0fe69ab0ac8caf77
Ali Jowhar <alijowharcoding@gmail.com>
2023-04-17 16:58:28 -0600

    added an eventListener to the gridElement div when its clicked it will change to black

--- main.js
+++ main.js
@@ -4,8 +4,10 @@ const container = document.querySelector(".mainContainer");
 
 for (let i = 0; i < 256; i++) {
   const div = document.createElement("div");
-
   div.className = "gridElement";
-
   container.appendChild(div);
+
+  div.addEventListener("click", function () {
+    div.style.backgroundColor = "black";
+  });
 }
