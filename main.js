9f13abfcb9099ee75d50515d9c6975375506abaa
Ali Jowhar <alijowharcoding@gmail.com>
2023-04-18 04:39:30 -0600

    seperated parts of js into functions for better read-abillity, added eventListener to change colors when hovering

--- main.js
+++ main.js
@@ -1,1 +1,22 @@
 "use strict";
+const board = document.querySelector(".board");
+
+document.addEventListener("DOMContentLoaded", function () {
+  createGrid(100);
+});
+
+const createGrid = function (size) {
+  board.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+  board.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+
+  for (let i = 0; i < size * size; i++) {
+    let div = document.createElement("div");
+
+    // div.style.border = "solid";
+    board.appendChild(div);
+
+    div.addEventListener("mouseover", function () {
+      div.style.backgroundColor = "black";
+    });
+  }
+};
