d70f2d0015d90d5406afb88a7a0518e12c93b575
Ali Jowhar <alijowharcoding@gmail.com>
2023-04-18 17:45:54 -0600

    added an erase button,and a rainbow button that uses a seprate function that creates random colors

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 "use strict";
 const board = document.querySelector(".board");
+const blackBtn = document.querySelector(".btn-dark");
+const rainbowBtn = document.querySelector(".btn-info");
+let popupBtn = document.getElementById("popup");
+const eraserBtn = document.querySelector(".btn-primary");
 
 document.addEventListener("DOMContentLoaded", function () {
   createGrid(16);
@@ -15,8 +19,32 @@ const createGrid = function (size) {
     div.style.border = "groove";
     board.appendChild(div);
 
-    div.addEventListener("mouseover", function () {
-      div.style.backgroundColor = "black";
+    blackBtn.addEventListener("click", () => {
+      div.addEventListener("mouseover", function () {
+        div.style.backgroundColor = "black";
+      });
+    });
+
+    rainbowBtn.addEventListener("click", () => {
+      div.addEventListener("mouseover", function () {
+        div.style.backgroundColor = getRandomColor();
+      });
+    });
+
+    eraserBtn.addEventListener("click", () => {
+      div.addEventListener("mouseover", function () {
+        div.style.backgroundColor = "white";
+      });
     });
   }
 };
+
+// create random colors for rainbow button
+function getRandomColor() {
+  var letters = "0123456789ABCDEF";
+  var color = "#";
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
